d94c9b54bc36c3717b27500bfe3b93c485fd49bf
qiangzhouliang <2538096489@qq.com>
2019-09-06 17:30:11 +0800

    强周亮：初始化项目

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -13,50 +13,11 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    Widget userHeader = UserAccountsDrawerHeader(
-      accountName: new Text('Tom'),
-      accountEmail: new Text('tom@xxx.com'),
-      currentAccountPicture: new CircleAvatar(
-        backgroundImage: AssetImage('images/pic1.jpg'), radius: 35.0,),);
-
+    //Scaffold 实现了基本的 Material Design 布局结构
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        leading: Builder(
-            builder: (BuildContext context){
-              return IconButton(
-                  icon: Icon(Icons.wifi_tethering),
-                  onPressed: (){
-                    Scaffold.of(context).openDrawer();
-                  }
-              );
-            }
-        ),
+      body: Center(
+        child: Text('我的'),
       ),
-      body: new Center(child: new Text('Home page'),),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            userHeader , // 可在这里替换自定义的header
-            ListTile(title: Text('Item 1'),
-              leading: new CircleAvatar(child: new Icon(Icons.school),),
-              onTap: () {
-                Navigator.pop(context);
-              },),
-            ListTile(title: Text('Item 2'),
-              leading: new CircleAvatar(child: new Text('B2'),),
-              onTap: () {
-                Navigator.pop(context);
-              },),
-            ListTile(title: Text('Item 3'),
-              leading: new CircleAvatar(
-                child: new Icon(Icons.list),),
-              onTap: () {
-                Navigator.pop(context);
-              },),
-          ],
-        ),
-      ),);
+    );
   }
 }
