26e29b28e9be954c92585cf715150482ee46577c
qiangzhouliang <2538096489@qq.com>
2019-09-06 23:13:45 +0800

    强周亮：添加屏幕适配组件

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //默认 width : 1080px , height:1920px , allowFontScaling:false
-//    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
-    //设置字体大小根据系统的“字体大小”辅助选项来进行缩放,默认为false
-//    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
