28f10fbc6f4f762dedb0eb4f72d5157488e35d81
qiangzhouliang <2538096489@qq.com>
2020-06-17 19:53:34 +0800

    强周亮：新增登录插件

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # flutter_framework
 
-一个flutter开发框架
+一个flutter开发框架，封装了两种类型的底部导航栏，普通导航栏和底部有加号的导航栏
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# 新增登录模块插件
--- flutter_login/lib/login.dart
+++ flutter_login/lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -12,28 +13,104 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin,Autom
   //重写keepAlive 为ture ，就是可以有记忆功能了。
   @override
   bool get wantKeepAlive => true;
+
+  //手机号的控制器
+  TextEditingController phoneController = TextEditingController();
+
+  //密码的控制器
+  TextEditingController passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     //Scaffold 实现了基本的 Material Design 布局结构
     return Scaffold(
         body:Container(
+          padding: const EdgeInsets.all(20.0),
           color: Colors.grey[200],
-          child: GestureDetector(
-            child: Center(
-              child: Text('登录',style: TextStyle(fontSize: 36.0),),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //输入用户名
+                TextField(
+                  controller: phoneController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.phone,),
+                    labelText: '请输入电话号码',
+                  ),
+                  autofocus: false,
+                ),
+                //输入密码
+                TextField(
+                  controller: passController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.lock),
+                    labelText: '请输入密码',
+                  ),
+                  autofocus: false,
+                ),
+                //登录
+                button_widget('登录',_login),
+              ],
             ),
-            onTap: () {
-              Fluttertoast.showToast(
-                msg: "确定要登录吗？",
-                toastLength: Toast.LENGTH_SHORT,
-                gravity: ToastGravity.CENTER,
-                backgroundColor: Colors.red,
-                textColor: Colors.white,
-                fontSize: 16.0
-              );
-            },
-          ),
+          )
         )
     );
+  }
+
+  //公共按钮组件
+  Padding button_widget(String title,VoidCallback onPressed,{Color color = Colors.blue,double minWidth = 200.0}) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+      child: ButtonTheme(
+        minWidth: minWidth,
+        height: 40.0,
+        child:  RaisedButton(
+          color: color,
+          textColor: Colors.white,
+          onPressed: onPressed,
+          child: Text(title),
+        ),
+      )
+    );
+  }
+
+  //登录方法
+  void _login(){
+    print({'user': phoneController.text,'pass':passController.text});
+    if(phoneController.text.length != 11) {
+      Fluttertoast.showToast(
+          msg: "请输入正确的手机号码",
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black87,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    } else if(passController.text.length == 0){
+      Fluttertoast.showToast(
+          msg: "密码不能为空",
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black87,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    } else {
+      Fluttertoast.showToast(
+          msg: "登录成功",
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black87,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      onTextClear();
+    }
+  }
+
+  //清空内容
+  void onTextClear() {
+    setState(() {
+      phoneController.clear();
+      passController.clear();
+    });
   }
 }
